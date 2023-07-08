2656b4b25dcc978b36fc47cd2b68857a5cda6a27
Leerena <leerena7@gmail.com>
2023-07-08 09:16:23 +0300

    lesson3

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import { MainPage } from "./Components/MainPage/MainPage";
 import { Root } from "./routes/Root";
 import { ErrorPage } from "./Components/ErrorPage/ErrorPage";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchNavigation } from "./features/navigationSlice";
+import { fetchColors } from "./features/colorSlice";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -11,17 +15,23 @@ const router = createBrowserRouter(
             <Route index element={<MainPage />} />
             <Route path='women' element={<MainPage gender='women' />} />
             <Route path='men' element={<MainPage gender='men' />} />
+            <Route path='kids' element={<MainPage gender='kids' />} />
             <Route path='women/:category' element={<MainPage gender='women' />} />
             <Route path='men/:category' element={<MainPage gender='men' />} />
+            <Route path='kids/:category' element={<MainPage gender='kids' />} />
             <Route path='*' element={<ErrorPage />} />
         </Route>
     )
 )
 
-export const App = () => (
-    <RouterProvider router={router}>
-        <Header />
-        <Footer />
-    </RouterProvider>
-);
+export const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchNavigation());
+        dispatch(fetchColors());
+    }, [dispatch]);
+
+    return <RouterProvider router={router}></RouterProvider>
+};
 
